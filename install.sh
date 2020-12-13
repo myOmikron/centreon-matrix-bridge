547ca3ad3fca9ddbc75685b11b79ce4d2f966055
myOmikron <accounts@omikron.dev>
2020-12-13 23:20:44 +0100

    Updated installer

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 adduser matrix
 cp -r centreon_matrix /home/matrix
+cp -r matrix_bridge /home/matrix
 cp centreon-matrix.service /usr/lib/systemd/system/
-ln -s /usr/lib/systemd/system/centreon-matrix.service /etc/systemd/system/multi-user.target.wants/
+ln -s /usr/lib/systemd/system/centreon-listener.service /etc/systemd/system/multi-user.target.wants/
+ln -s /usr/lib/systemd/system/matrix-handler.service /etc/systemd/system/multi-user.target.wants/
 systemctl daemon-reload
-systemctl enable centreon-matrix
+systemctl enable centreon-listener.service
+systemctl enable matrix-handler.service
 cd /home/matrix && python3 -m venv venv
-venv/bin/python -m pip install -U pip hopfenmatrix
+venv/bin/python -m pip install -U pip hopfenmatrix Django
