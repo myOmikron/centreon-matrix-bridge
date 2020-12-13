a72b03909c4510d503d0c4ce316b2b46b5841aaa
myOmikron <accounts@omikron.dev>
2020-12-13 23:18:28 +0100

    Added matrix bridge

---
+++ matrix_bridge/main.py
@@ -0,0 +1,36 @@
+import asyncio
+import json
+
+from hopfenmatrix.config import Namespace, Config
+from hopfenmatrix.matrix import MatrixBot
+
+
+async def listener(bot):
+    while True:
+        with open("../handler.fifo") as fh:
+            lines = fh.readlines()
+            for line in lines:
+                res = json.loads(line)
+                await bot.send_message(
+                    message=res.content,
+                    room_id=res.message["target"],
+                    formatted_message=res.content_formatted
+                )
+
+
+class MyConfig(Config):
+    def __init__(self):
+        super(MyConfig, self).__init__()
+        centreon = Namespace()
+        centreon.shared_secret = ""
+
+
+async def main():
+    bot = MatrixBot(display_name="Centreon Notification", config_class=MyConfig)
+    bot.set_auto_join()
+    bot.add_coroutine_callback(listener(bot))
+    await bot.start_bot()
+
+
+if __name__ == '__main__':
+    asyncio.get_event_loop().run_until_complete(main())
